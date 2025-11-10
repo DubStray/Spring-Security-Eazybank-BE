90791025fad248a4c56f552e4421c35e4ce5de48
DubStray <francesco.gaming00@gmail.com>
2025-11-10 16:20:21 +0100

    Added JPA properties in application.properties, added new dependencies in the pom, disabled CSRF protection and added an API in the permitAll method. New class Customer and the relative repository, new custom UserDetailService with custom loadUserByUsername and new UserController.

--- src/main/java/com/eazybytes/springsecsection1/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/springsecsection1/config/ProjectSecurityConfig.java
@@ -4,13 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
@@ -28,34 +23,16 @@ public class ProjectSecurityConfig {
      */
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests
+        http
+                .csrf(csrfConfig -> csrfConfig.disable()) // Disabilito la protezione CSRF TEMPORANEAMENTE (Blocca richieste POST HTTP)
+                .authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
-                .requestMatchers("/notices", "/contact", "/error").permitAll());
+                .requestMatchers("/notices", "/contact", "/error", "/register").permitAll());
 //        http.formLogin(formLoginConf -> formLoginConf.disable());
 //        http.httpBasic(httpBasicConf -> httpBasicConf.disable());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
-    }
-
-    /*
-    Metodo temporaneo (presumo) dove "creare" in memoria, e quindi senza un DB, degli user conb relativi ruoli.
-    Si settano lo username, password, autorizazzioni e alla fine si crea con il .build()
-     */
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withUsername("user")
-                        .password("{noop}EazyBytes@12345")
-                        .authorities("read")
-                        .build();
-        UserDetails admin =
-                User.withUsername("admin")
-                        .password("{bcrypt}$2a$12$X8HQD1QlCYXIafrYSoDZveRhd7wJufKA.sdLS4beiP5em0fUP9odO")
-                        .authorities("admin")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
     }
 
     /*
