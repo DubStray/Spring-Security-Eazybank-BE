febc6f3cce1056d2b8b38ead6fa0c0169d8da3be
DubStray <francesco.gaming00@gmail.com>
2025-11-24 13:56:38 +0100

    All controller improved with adeguate API logic. Added new security logic to the ProjectSecurityConfig. Added adeguate models class alongside with JPA Repositories and Lombok library. Created a custom CsrfCookieFIlter class.

---
+++ src/main/java/com/eazybytes/eazybankbackend/repository/CardsRepository.java
@@ -0,0 +1,14 @@
+package com.eazybytes.eazybankbackend.repository;
+
+import com.eazybytes.eazybankbackend.model.Cards;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CardsRepository extends CrudRepository<Cards, Long> {
+
+    List<Cards> findByCustomerId(long customerId);
+
+}
---
+++ src/main/java/com/eazybytes/eazybankbackend/repository/LoanRepository.java
@@ -0,0 +1,14 @@
+package com.eazybytes.eazybankbackend.repository;
+
+import com.eazybytes.eazybankbackend.model.Loans;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LoanRepository extends CrudRepository<Loans, Long> {
+
+    List<Loans> findByCustomerIdOrderByStartDtDesc(long customerId);
+
+}
