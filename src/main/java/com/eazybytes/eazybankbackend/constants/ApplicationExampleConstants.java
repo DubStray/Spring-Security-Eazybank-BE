fed8d2976f4176dc1234f94f3b409ae7816fe470
DubStray <francesco.gaming00@gmail.com>
2025-12-04 18:40:12 +0100

    Removed other sensitive files

--- src/main/java/com/eazybytes/eazybankbackend/constants/ApplicationExampleConstants.java
+++ src/main/java/com/eazybytes/eazybankbackend/constants/ApplicationExampleConstants.java
@@ -1,4 +1,13 @@
 package com.eazybytes.eazybankbackend.constants;
 
+/**
+ * Example class that contains constants for JWT Secrets
+ */
 public class ApplicationExampleConstants {
+
+    public static final String JWT_SECRET_KEY = "YOUR_JWT_SECRET";
+
+    public static final String JWT_SECRET_DEFAULT_VALUE = "YOUR_JWT_SECRET";
+
+    public static final String JWT_HEADER = "YOUR_JWT_HEADER";
 }
